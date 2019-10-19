d08a8b13cac46ee47e04a672de4d044980881317
NimRo97 <nimronagy@gmail.com>
2019-10-19 15:39:01 +0200

    Initial branch commit

--- src/mnemonics.c
+++ src/mnemonics.c
@@ -2,3 +2,11 @@
 #include <string.h>
 
 #include "mnemonics.h"
+
+
+int to_mnemonic(const struct dictionary *dict,
+                const unsigned char *bytes, size_t bytes_l,
+                const unsigned char *delim, size_t delim_l,
+                unsigned char *output, size_t *output_l) {
+    return 2;
+}
